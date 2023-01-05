338eb1315787cc40ae34b77705b93173efc693d6
luismariovaldiviesos <luismario.valdiviesos@gmail.com>
2023-01-05 16:28:47 -0500

    ToDo

--- resources/views/layouts/theme/sidebar.blade.php
+++ resources/views/layouts/theme/sidebar.blade.php
@@ -151,7 +151,7 @@
 
 
                 <li>
-                    <a href="{{ route('monitores') }}" class="side-menu">
+                    <a href="#" class="side-menu">
                         <div class="side-menu__icon"> <i data-feather="settings"></i> </div>
                         <div class="side-menu__title"> Monitor  </div>
                     </a>
--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::middleware(['auth'])->group(function (){
     Route::get('dash', Dashboard::class)->name('dash');
 
 
-    Route::get('monitores', Monitores::class)->name('monitores');
+
 
 });
 
