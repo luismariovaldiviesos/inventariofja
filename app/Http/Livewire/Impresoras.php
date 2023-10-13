7701a6979657b686780c49bf5b883c30094d0334
luismariovaldiviesos <luismario.valdiviesos@gmail.com>
2023-10-13 10:18:04 -0500

    paginate en printer

--- app/Http/Livewire/Impresoras.php
+++ app/Http/Livewire/Impresoras.php
@@ -64,7 +64,7 @@ class Impresoras extends Component
 
         $info = Impresora::join('users as u','u.id','impresoras.user_id')
         ->select('impresoras.*','u.name as usuario')
-            ->get($this->pagination);
+            ->paginate($this->pagination);
 
 
         return view('livewire.impresoras.component', [
